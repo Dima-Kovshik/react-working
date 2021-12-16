dca8b8954fc9d391e7f9534a753a71e0d477a5c0
Dima-Kovshik <95184231+Dima-Kovshik@users.noreply.github.com>
2021-12-16 15:20:23 +0300

    Add files via upload

--- src/comp/Catalog.js
+++ src/comp/Catalog.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import firebase from "./firebase";
+import CatalogItem from "./CatalogItem";
 
 export default function Catalog() {
   const [products, setProducts] = useState([]);
@@ -32,32 +33,7 @@ export default function Catalog() {
 
       {products.map((product) => (
 
-        <div class="product-wrap">
-
-          <div class="product-image">
-            <a href=""><img src="https://html5book.ru/wp-content/uploads/2015/10/black-dress.jpg" /></a>
-            <div class="shadow"></div>
-            <div class="actions">
-              <div class="actions-btn">
-                <div class="add-to-cart">
-                  <a class="add-to-cart-button" href="#" title="Добавить в корзину"></a>
-                </div>
-                <div class="add-to-links">
-
-                  <div class="quikview">
-                    <a key={product.id} class="quikview-button" href="#" title="Быстрый просмотр"></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="product-list">
-            <h3>{product.name}</h3>
-            <div class="price">&#8381; {product.price}</div>
-
-          </div>
-
-        </div>
+        <CatalogItem name={product.name} price={product.price} id={product.id} />
 
       ))}
     </div>
